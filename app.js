97a2e99cc9ed9d56cbde6999084b4f86d6e5485f
Ivan Day <ivanday@utexas.edu>
2022-11-07 11:18:57 -0600

    Implement basic Grocery Store list that is not interactive using different components

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // TODO
+const App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList />
+  </div>
+);
+
+const GroceryList = (props) => {
+  return <div><Cucumbers /><Kale /></div>;
+}
+
+const Cucumbers = () => (
+  <li>Cucumbers</li>
+);
+
+const Kale = () => (
+  <li>Kale</li>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
